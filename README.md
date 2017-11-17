642c5d066bc93b901bb811732220feea15e53f34
Tim Retz <human.shield.117@gmail.com>
2017-11-17 02:12:51 -0800

    first commit, moved everything over from old style

---
+++ README.md
@@ -0,0 +1,3 @@
+This is just going to be my collection of dot-files.
+
+install it all by running "./installer.sh"
---
+++ installer.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+# Neovim
+mkdir -p $HOME/.config/nvim
+cp -R ./neovim/* $HOME/.config/nvim
+nvim +PlugInstall +qall
+
+# Ranger
+cp -R ./ranger $HOME/.config/ranger
+
+# Zsh
+cp ./zsh/zshrc $HOME/.zshrc
+cp ./zsh/zshenv $HOME/.zshenv
+
+# Oh My Zsh
+mkdir -p $HOME/.oh-my-zsh
+cp -R ./oh-my-zsh $HOME/.oh-my-zsh
+
+# Tmux
+cp ./tmux.conf ~/.tmux.conf
+
+# Utilities
+cp ./gitconfig ~/.gitconfig
